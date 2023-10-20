62d410bbc65845c23b96284cbf22e46b3b7b1b71
Taimur Hassan <70101972+taimurhassan@users.noreply.github.com>
2023-10-20 14:44:10 +0400

    Add files via upload

---
+++ metrics/stats_utils.py
@@ -0,0 +1,335 @@
+import warnings
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+from scipy.optimize import linear_sum_assignment
+
+
+def get_fast_aji(true, pred):
+    
+    true = np.copy(true)  # ? do we need this
+    pred = np.copy(pred)
+    true_id_list = list(np.unique(true))
+    pred_id_list = list(np.unique(pred))
+
+    true_masks = [
+        None,
+    ]
+    for t in true_id_list[1:]:
+        t_mask = np.array(true == t, np.uint8)
+        true_masks.append(t_mask)
+
+    pred_masks = [
+        None,
+    ]
+    for p in pred_id_list[1:]:
+        p_mask = np.array(pred == p, np.uint8)
+        pred_masks.append(p_mask)
+
+    pairwise_inter = np.zeros(
+        [len(true_id_list) - 1, len(pred_id_list) - 1], dtype=np.float64
+    )
+    pairwise_union = np.zeros(
+        [len(true_id_list) - 1, len(pred_id_list) - 1], dtype=np.float64
+    )
+
+    for true_id in true_id_list[1:]:  # 0-th is background
+        t_mask = true_masks[true_id]
+        pred_true_overlap = pred[t_mask > 0]
+        pred_true_overlap_id = np.unique(pred_true_overlap)
+        pred_true_overlap_id = list(pred_true_overlap_id)
+        for pred_id in pred_true_overlap_id:
+            if pred_id == 0:  # ignore
+                continue  # overlaping background
+            p_mask = pred_masks[pred_id]
+            total = (t_mask + p_mask).sum()
+            inter = (t_mask * p_mask).sum()
+            pairwise_inter[true_id - 1, pred_id - 1] = inter
+            pairwise_union[true_id - 1, pred_id - 1] = total - inter
+
+    pairwise_iou = pairwise_inter / (pairwise_union + 1.0e-6)
+    paired_pred = np.argmax(pairwise_iou, axis=1)
+    pairwise_iou = np.max(pairwise_iou, axis=1)
+    paired_true = np.nonzero(pairwise_iou > 0.0)[0]
+    paired_pred = paired_pred[paired_true]
+    overall_inter = (pairwise_inter[paired_true, paired_pred]).sum()
+    overall_union = (pairwise_union[paired_true, paired_pred]).sum()
+
+    paired_true = list(paired_true + 1)  
+    paired_pred = list(paired_pred + 1)
+    unpaired_true = np.array(
+        [idx for idx in true_id_list[1:] if idx not in paired_true]
+    )
+    unpaired_pred = np.array(
+        [idx for idx in pred_id_list[1:] if idx not in paired_pred]
+    )
+    for true_id in unpaired_true:
+        overall_union += true_masks[true_id].sum()
+    for pred_id in unpaired_pred:
+        overall_union += pred_masks[pred_id].sum()
+
+    aji_score = overall_inter / overall_union
+    return aji_score
+
+
+#####
+def get_fast_aji_plus(true, pred):
+   
+    true = np.copy(true)  # ? do we need this
+    pred = np.copy(pred)
+    true_id_list = list(np.unique(true))
+    pred_id_list = list(np.unique(pred))
+
+    true_masks = [
+        None,
+    ]
+    for t in true_id_list[1:]:
+        t_mask = np.array(true == t, np.uint8)
+        true_masks.append(t_mask)
+
+    pred_masks = [
+        None,
+    ]
+    for p in pred_id_list[1:]:
+        p_mask = np.array(pred == p, np.uint8)
+        pred_masks.append(p_mask)
+
+    pairwise_inter = np.zeros(
+        [len(true_id_list) - 1, len(pred_id_list) - 1], dtype=np.float64
+    )
+    pairwise_union = np.zeros(
+        [len(true_id_list) - 1, len(pred_id_list) - 1], dtype=np.float64
+    )
+
+    for true_id in true_id_list[1:]:  # 0-th is background
+        t_mask = true_masks[true_id]
+        pred_true_overlap = pred[t_mask > 0]
+        pred_true_overlap_id = np.unique(pred_true_overlap)
+        pred_true_overlap_id = list(pred_true_overlap_id)
+        for pred_id in pred_true_overlap_id:
+            if pred_id == 0:  # ignore
+                continue  # overlaping background
+            p_mask = pred_masks[pred_id]
+            total = (t_mask + p_mask).sum()
+            inter = (t_mask * p_mask).sum()
+            pairwise_inter[true_id - 1, pred_id - 1] = inter
+            pairwise_union[true_id - 1, pred_id - 1] = total - inter
+    #
+    pairwise_iou = pairwise_inter / (pairwise_union + 1.0e-6)
+    paired_true, paired_pred = linear_sum_assignment(-pairwise_iou)
+    paired_iou = pairwise_iou[paired_true, paired_pred]
+    paired_true = paired_true[paired_iou > 0.0]
+    paired_pred = paired_pred[paired_iou > 0.0]
+    paired_inter = pairwise_inter[paired_true, paired_pred]
+    paired_union = pairwise_union[paired_true, paired_pred]
+    paired_true = list(paired_true + 1)  
+    paired_pred = list(paired_pred + 1)
+    overall_inter = paired_inter.sum()
+    overall_union = paired_union.sum()
+    unpaired_true = np.array(
+        [idx for idx in true_id_list[1:] if idx not in paired_true]
+    )
+    unpaired_pred = np.array(
+        [idx for idx in pred_id_list[1:] if idx not in paired_pred]
+    )
+    for true_id in unpaired_true:
+        overall_union += true_masks[true_id].sum()
+    for pred_id in unpaired_pred:
+        overall_union += pred_masks[pred_id].sum()
+    #
+    aji_score = overall_inter / overall_union
+    return aji_score
+
+
+#####
+def get_fast_pq(true, pred, match_iou=0.5):
+    
+    assert match_iou >= 0.0, "Cant' be negative"
+
+    true = np.copy(true)
+    pred = np.copy(pred)
+    true_id_list = list(np.unique(true))
+    pred_id_list = list(np.unique(pred))
+
+    true_masks = [
+        None,
+    ]
+    for t in true_id_list[1:]:
+        t_mask = np.array(true == t, np.uint8)
+        true_masks.append(t_mask)
+
+    pred_masks = [
+        None,
+    ]
+    for p in pred_id_list[1:]:
+        p_mask = np.array(pred == p, np.uint8)
+        pred_masks.append(p_mask)
+
+    pairwise_iou = np.zeros(
+        [len(true_id_list) - 1, len(pred_id_list) - 1], dtype=np.float64
+    )
+
+    for true_id in true_id_list[1:]:  # 0-th is background
+        t_mask = true_masks[true_id]
+        pred_true_overlap = pred[t_mask > 0]
+        pred_true_overlap_id = np.unique(pred_true_overlap)
+        pred_true_overlap_id = list(pred_true_overlap_id)
+        for pred_id in pred_true_overlap_id:
+            if pred_id == 0:  # ignore
+                continue  # overlaping background
+            p_mask = pred_masks[pred_id]
+            total = (t_mask + p_mask).sum()
+            inter = (t_mask * p_mask).sum()
+            iou = inter / (total - inter)
+            pairwise_iou[true_id - 1, pred_id - 1] = iou
+    
+    if match_iou >= 0.5:
+        paired_iou = pairwise_iou[pairwise_iou > match_iou]
+        pairwise_iou[pairwise_iou <= match_iou] = 0.0
+        paired_true, paired_pred = np.nonzero(pairwise_iou)
+        paired_iou = pairwise_iou[paired_true, paired_pred]
+        paired_true += 1  # index is instance id - 1
+        paired_pred += 1  # hence return back to original
+    else:  
+        paired_true, paired_pred = linear_sum_assignment(-pairwise_iou)
+
+        paired_iou = pairwise_iou[paired_true, paired_pred]
+
+        paired_true = list(paired_true[paired_iou > match_iou] + 1)
+        paired_pred = list(paired_pred[paired_iou > match_iou] + 1)
+        paired_iou = paired_iou[paired_iou > match_iou]
+
+    unpaired_true = [idx for idx in true_id_list[1:] if idx not in paired_true]
+    unpaired_pred = [idx for idx in pred_id_list[1:] if idx not in paired_pred]
+    tp = len(paired_true)
+    fp = len(unpaired_pred)
+    fn = len(unpaired_true)
+    # get the F1-score i.e DQ
+    dq = tp / (tp + 0.5 * fp + 0.5 * fn)
+    # get the SQ, no paired has 0 iou so not impact
+    sq = paired_iou.sum() / (tp + 1.0e-6)
+
+    return [dq, sq, dq * sq], [paired_true, paired_pred, unpaired_true, unpaired_pred]
+
+
+#####
+def get_fast_dice_2(true, pred):
+    """Ensemble dice."""
+    true = np.copy(true)
+    pred = np.copy(pred)
+    true_id = list(np.unique(true))
+    pred_id = list(np.unique(pred))
+
+    overall_total = 0
+    overall_inter = 0
+
+    true_masks = [np.zeros(true.shape)]
+    for t in true_id[1:]:
+        t_mask = np.array(true == t, np.uint8)
+        true_masks.append(t_mask)
+
+    pred_masks = [np.zeros(true.shape)]
+    for p in pred_id[1:]:
+        p_mask = np.array(pred == p, np.uint8)
+        pred_masks.append(p_mask)
+
+    for true_idx in range(1, len(true_id)):
+        t_mask = true_masks[true_idx]
+        pred_true_overlap = pred[t_mask > 0]
+        pred_true_overlap_id = np.unique(pred_true_overlap)
+        pred_true_overlap_id = list(pred_true_overlap_id)
+        try:  # blinly remove background
+            pred_true_overlap_id.remove(0)
+        except ValueError:
+            pass  # just mean no background
+        for pred_idx in pred_true_overlap_id:
+            p_mask = pred_masks[pred_idx]
+            total = (t_mask + p_mask).sum()
+            inter = (t_mask * p_mask).sum()
+            overall_total += total
+            overall_inter += inter
+
+    return 2 * overall_inter / overall_total
+
+
+def get_dice_1(true, pred):
+    """Traditional dice."""
+    # cast to binary 1st
+    true = np.copy(true)
+    pred = np.copy(pred)
+    true[true > 0] = 1
+    pred[pred > 0] = 1
+    inter = true * pred
+    denom = true + pred
+    return 2.0 * np.sum(inter) / np.sum(denom)
+
+
+####
+def get_dice_2(true, pred):
+    true = np.copy(true)
+    pred = np.copy(pred)
+    true_id = list(np.unique(true))
+    pred_id = list(np.unique(pred))
+    # remove background aka id 0
+    true_id.remove(0)
+    pred_id.remove(0)
+
+    total_markup = 0
+    total_intersect = 0
+    for t in true_id:
+        t_mask = np.array(true == t, np.uint8)
+        for p in pred_id:
+            p_mask = np.array(pred == p, np.uint8)
+            intersect = p_mask * t_mask
+            if intersect.sum() > 0:
+                total_intersect += intersect.sum()
+                total_markup += t_mask.sum() + p_mask.sum()
+    return 2 * total_intersect / total_markup
+
+
+#####
+def remap_label(pred, by_size=False):
+   
+    pred_id = list(np.unique(pred))
+    pred_id.remove(0)
+    if len(pred_id) == 0:
+        return pred  # no label
+    if by_size:
+        pred_size = []
+        for inst_id in pred_id:
+            size = (pred == inst_id).sum()
+            pred_size.append(size)
+        pair_list = zip(pred_id, pred_size)
+        pair_list = sorted(pair_list, key=lambda x: x[1], reverse=True)
+        pred_id, pred_size = zip(*pair_list)
+
+    new_pred = np.zeros(pred.shape, np.int32)
+    for idx, inst_id in enumerate(pred_id):
+        new_pred[pred == inst_id] = idx + 1
+    return new_pred
+
+
+#####
+def pair_coordinates(setA, setB, radius):
+   
+    pair_distance = scipy.spatial.distance.cdist(setA, setB, metric='euclidean')
+
+    # * Munkres pairing with scipy library
+    # the algorithm return (row indices, matched column indices)
+    # if there is multiple same cost in a row, index of first occurence 
+    # is return, thus the unique pairing is ensured
+    indicesA, paired_indicesB = linear_sum_assignment(pair_distance)
+
+    # extract the paired cost and remove instances 
+    # outside of designated radius
+    pair_cost = pair_distance[indicesA, paired_indicesB]
+
+    pairedA = indicesA[pair_cost <= radius]
+    pairedB = paired_indicesB[pair_cost <= radius]
+
+    pairing = np.concatenate([pairedA[:,None], pairedB[:,None]], axis=-1)
+    unpairedA = np.delete(np.arange(setA.shape[0]), pairedA)
+    unpairedB = np.delete(np.arange(setB.shape[0]), pairedB)
+    return pairing, unpairedA, unpairedB
